0ba45b19fa530eee74d66b464f7d0dcb82de02f9
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-06-29 22:43:38 +0530

    Refix header

--- findus/includes/header.php
+++ findus/includes/header.php
@@ -1,59 +1,101 @@
-  <header id="header">
+<header id="header">
 
-    <!--BURGER NAVIGATION SECTION START-->
+   <!--BURGER NAVIGATION SECTION START-->
 
-    <!--BURGER NAVIGATION SECTION END-->
+   <!--BURGER NAVIGATION SECTION END-->
 
-    <div class="container">
+   <div class="container">
 
-      <!--NAVIGATION START-->
+     <!--NAVIGATION START-->
 
-      <div class="navigation-col">
+     <div class="navigation-col">
 
-        <nav class="navbar navbar-inverse">
+       <nav class="navbar navbar-inverse">
 
-          <div class="navbar-header">
+         <div class="navbar-header">
 
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
 
-            <h2><strong class="logo" style="color: purple; padding-top:8%">FIND US</strong></h2> </div>
+           <h2><strong class="logo" style="color: purple; padding-top:8%">FIND US</strong></h2> </div>
 
-          <div id="navbar" class="collapse navbar-collapse">
+         <div id="navbar" class="collapse navbar-collapse">
 
-            <ul class="nav navbar-nav" id="nav">
+           <ul class="nav navbar-nav" id="nav">
 
-              <li><a href="index.php">Home</a>
+             <li><a href="index.php">Home</a>
 
 
 
-              </li>
+             </li>
 <?php if (strlen($_SESSION['jsid']==0)) {?>
-              <li><a href="sign-up.php">Jobseekers</a>
-              </li>
+             <li><a href="sign-up.php">Jobseekers</a>
+             </li>
 
-              <li><a href="employers/emp-login.php">Domestic Workers</a></li>
+             <li><a href="employers/emp-login.php">Domestic Workers</a></li>
 <li><a href="admin/index.php">Admin</a></li><?php } ?>
 
 
 <?php if (strlen($_SESSION['jsid']!=0)) {?>
-                  <li><a href="applied-jobs.php">History of Applied Jobs</a></li>
+                 <li><a href="applied-jobs.php">History of Applied Jobs</a></li>
 <?php } ?>
-              </li>
-            <li><a href="Chat/index.php" target="_blank">Chat</a></li>
-            </ul>
+             </li>
+           <li><a href="Chat/index.php" target="_blank">Chat</a></li>
+           </ul>
 
-          </div>
+         </div>
 
-        </nav>
+       </nav>
 
-      </div>
+     </div>
 
-      <!--NAVIGATION END-->
+     <!--NAVIGATION END-->
 
-    </div>
+   </div>
 
 
-    </div>
 
-    <!--USER OPTION COLUMN END-->
-  </header>
+   <!--USER OPTION COLUMN START-->
+
+   <div class="user-option-col">
+
+     <div class="thumb">
+
+       <div class="dropdown">
+<?php if (strlen($_SESSION['jsid']!=0)) {?>
+
+  <?php
+        $uid= $_SESSION['jsid'];
+$sql="SELECT * from tbljobseekers where id='$uid'";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{               ?>
+         <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"> <img src="images/<?php echo $row->ProfilePic;?>" width="40" height="40" ></button>
+<?php $cnt=$cnt+1;}} ?>
+         <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+
+           <li><a href="my-profile.php">My Profile</a></li>
+
+           <li><a href="change-password.php">Change Password</a></li>
+
+           <li><a href="profile.php">Edit Profile</a></li>
+
+           <li><a href="logout.php">Log Out</a></li>
+
+         </ul>
+<?php } ?>
+       </div>
+
+     </div>
+
+
+
+   </div>
+
+   <!--USER OPTION COLUMN END-->
+ </header>
